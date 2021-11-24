c39d6c7f26803f8165807585e28af53ed0239ebb
jeroenbaas@gmail.com <jeroenbaas@gmail.com>
2021-11-24 19:21:40 +0000

    add partition size to config

--- notebooks/config-example.py
+++ notebooks/config-example.py
@@ -6,3 +6,15 @@
 
 # Where are the open-alex files?
 base_path='dbfs:/path/to/your/mounted/dataset/openalex/data_dump_v1/2021-10-11/'
+
+# COMMAND ----------
+
+# sizes for the partitions of the out-tables
+partition_sizes={
+  'author':1000,
+  'affiliation':10,
+  'fieldofstudy':10,
+  'paper':1000,
+  'journal':10,
+  'default':200
+}
